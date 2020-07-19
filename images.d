3d3b5f927c89c1e44237d539742202b4558ff44f
ssoraka <t89043139690@mail.ru>
2020-07-19 23:06:33 +0300

    поправил клавиши и добавил их выбор в для ОС

--- images.d
+++
@@ -1,4 +0,0 @@
-images.o: images.c includes/ft_fdf.h includes/../libft/libft.h \
- includes/../libft/collections_header.h includes/../libft/libft.h \
- includes/../libft/collections_struct.h includes/ft_buttons.h \
- libs/minilibx/mlx.h includes/ft_fdf_struct.h includes/ft_fdf.h
